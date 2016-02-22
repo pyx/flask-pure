43327532c2ff3a26e9b74877aa4bebe534df258c
Philip Xu <pyx@xrefactor.com>
2016-02-21 20:37:12 -0500

    Added setup script

---
+++ setup.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+import re
+import sys
+from os import path
+from setuptools import setup
+
+ROOT_DIR = path.abspath(path.dirname(__file__))
+sys.path.insert(0, ROOT_DIR)
+
+DESCRIPTION = 'Flask-Pure - a Flask extension for Pure.css'
+LONG_DESCRIPTION = open(path.join(ROOT_DIR, 'README.rst')).read()
+VERSION = re.search(
+    "__version__ = '([^']+)'",
+    open(path.join(ROOT_DIR, 'flask_pure', '__init__.py')).read()
+).group(1)
+
+
+setup(
+    name='Flask-Pure',
+    version=VERSION,
+    url='https://github.com/pyx/flask-pure/',
+    license='BSD-New',
+    author='Philip Xu',
+    author_email='pyx@xrefactor.com',
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=['flask_pure'],
+    include_package_data=True,
+    zip_safe=False,
+    platforms='any',
+    install_requires=[
+        'Flask>=0.10',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
