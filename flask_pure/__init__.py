2507b5ef5665f7e7f92f52c308b6c0b89e0167ab
Philip Xu <pyx@xrefactor.com>
2016-02-21 20:36:40 -0500

    Added package flask_pure

---
+++ flask_pure/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2016, Philip Xu <pyx@xrefactor.com>
+# License: BSD New, see LICENSE for details.
+"""Flask-Pure - a Flask extension for Pure.css"""
+
+__version__ = '0.1.dev0'
